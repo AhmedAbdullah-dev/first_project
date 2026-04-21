2c37f71f58bd8543a2e28d19bea6f405383509f6
Ahmed <ahmedabdullah45379@gmail.com>
2026-04-22 00:01:22 +0200

    pull requst

--- app.js
+++ app.js
@@ -9,3 +9,4 @@ console.log(`my name is ${name} and my age is ${age}`);
 console.log(`i am from libya from seedy alseah city`);
 console.log(`as;lkdfjakfjas;lfkjas;fkjasfl;skjfsjf`);
 console.log(20 + 30);
+console.log(`hello world`);
