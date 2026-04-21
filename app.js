4e08c8013d3edca032785de611071d14758095b4
Ahmed <ahmedabdullah45379@gmail.com>
2026-04-21 23:48:17 +0200

    forth edit

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ let name = 'Ahmed';
 let age = 20;
 let space = ' ';
 
+
 // console.log(name +' '+ age);
 
 console.log(`my name is ${name} and my age is ${age}`);
 console.log(`i am from libya from seedy alseah city`);
 console.log(`as;lkdfjakfjas;lfkjas;fkjasfl;skjfsjf`);
+console.log(20 + 30);
